4908cba652f15fc77d6ea39be3412bce3f04e23a
Apple <apple@Apples-MacBook-Pro-94.local>
2019-06-21 11:23:42 -0400

    iteration two

--- emojiFeels/ViewController.swift
+++ emojiFeels/ViewController.swift
@@ -10,19 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emojis = [ "😃": "Glad to see you're happy! Spread your love to the world!", "🥰" : "Oh look at you love bird! Be careful not to let your passion cloud your judgement!", "🤪" : "Try and put your energy to productive means today.", "😔" : "Aw, brighten up buttercup! Find something to be grateful about today"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func showMessage(sender : UIButton)
     {
+        
         //when a button is clicked print an inspirational message related to the emoji
         //or fo x in index x print...
-        let alertController = UIAlertController(title: "Here's a message for you!", message: "How are you feeling today?", preferredStyle: UIAlertController.Style.alert)
+        @IBAction func showMessage(sender: UIButton)
+        {
+            let selectedEmotion = sender.titleLabel?.text
+        
+        let alertController = UIAlertController(title: "Here's a message for you!", message: emojis.values, preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            
+            
         
         present(alertController, animated: true, completion: nil)
+        }
 
     }
 
